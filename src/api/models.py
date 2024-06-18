a10704aada11bc0f54f735e3b831dcf3ab7c1b7c
Andrés Salamanca <141695644+asalam3312@users.noreply.github.com>
2024-06-18 17:00:56 +0000

    feat/ route user/signup created

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(9999), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -16,4 +17,20 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Manager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(9999), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
+    phone_number= db.Column(db.Integer, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Manager {self.email}'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Manager
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,41 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/User/login', methods=['GET'])
+def user_login():
+    response_body={
+        "message": "This route is working, go ahead an create an user"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/user/signup', methods=['POST'])
+def user_signup():
+    try:
+        email = request.json.get('email')
+        password = request.json.get('password')
+        phone_number = request.json.get('phone_number')
+        
+
+        if not email or not password or not phone_number:
+            return jsonify({"Error": "email, password, phone number and is active are required"}), 400
+        
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return jsonify({"msg": "This email is alredy registered"}), 409
+        
+        password_hash = generate_password_hash(password)
+
+        new_user= User(
+            email = email,
+            password = password_hash,
+            phone_number = phone_number
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"Success": "user created successfully"}), 201
+    
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 500
