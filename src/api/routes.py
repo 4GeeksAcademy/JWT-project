6f89868d75a8fdabd20968aa26903f47142d1715
Andrés Salamanca <141695644+asalam3312@users.noreply.github.com>
2024-06-20 00:17:45 +0000

    feat/ routes signup and login created

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,16 +16,7 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
-
+# --------------User-----------------------------------
 @api.route('/user/signup', methods=['POST'])
 def user_signup():
     try:
@@ -35,7 +26,7 @@ def user_signup():
         
 
         if not email or not password or not phone_number:
-            return jsonify({"Error": "email, password, phone number and is active are required"}), 400
+            return jsonify({"Error": "email, password, phone number are required"}), 400
         
         existing_user = User.query.filter_by(email=email).first()
         if existing_user:
@@ -87,3 +78,60 @@ def user_login():
     except Exception as e:
         return jsonify({"Error", str(e)})
         
+# ---------------------- Manager ----------------------
+@api.route('/manager/signup', methods=['POST'])
+def manager_signup():
+    try:
+        email = request.json.get('email')
+        password = request.json.get('password')
+        phone_number = request.json.get('phone_number')
+
+        if not email or not password or not phone_number:
+            return jsonify({"Error": "email, password and phone number are required"}), 400
+        
+        existing_manager = Manager.query.filter_by(email=email).first()
+        
+        if existing_manager:
+            return jsonify({"msg": "this email is already registered"}), 409
+        
+        password_hash = generate_password_hash(password)
+        new_manager = Manager(
+            email=email,
+            password=password_hash,
+            phone_number=phone_number
+        )
+        db.session.add(new_manager)
+        db.session.commit()
+        return jsonify({"success": "Manager has been created successfully"})
+    except Exception as e:
+        return jsonify({"Error": "Problems creating manager profile " + str(e)}), 500
+
+@api.route('/manager/login', methods=['POST'])
+def manager_login():
+    try:
+        email = request.json.get('email')
+        password = request.json.get('password')
+
+        if not email or not password:
+            return jsonify({"error": "email and password are requiered"}), 400
+        
+        manager_profile = Manager.query.filter_by(email=email).first()
+        if not manager_profile:
+            return jsonify({"Error": "Invalid email"}), 400
+        
+        password_from_db = manager_profile.password
+
+        manager_access = check_password_hash(password_from_db, password)
+
+        if manager_access:
+            expires = timedelta(hours=3)
+            manager_id = manager_profile.id
+            access_token = create_access_token(identity=manager_id, expires_delta=expires)
+            return jsonify({"access to token": access_token, "message": "login successfully"}), 200
+        else:
+            return jsonify({"Error": "incorrect password"}),400
+        
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        return jsonify({"Error": "Database error", "message": str(e)}),500
+    except Exception as e:
+        return jsonify({"Error": str(e)}),500
